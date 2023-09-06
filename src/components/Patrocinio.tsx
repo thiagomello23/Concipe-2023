d9da8468b3fbf704b9f3cf03b1f03c1b3dc3c820
thiagomello23 <thiagomellodiniz@hotmail.com>
2023-09-06 14:45:11 -0400

    update: Atualizando o tamanho das logos

--- src/components/Patrocinio.tsx
+++ src/components/Patrocinio.tsx
@@ -5,13 +5,13 @@ import DentalNorte from "../assets/logo-Dental-norte.png"
 
 export default function Patrocinio() {
   return (
-    <div className='bg-white p-12 py-20 flex justify-center items-center flex-col'>
+    <div className='bg-white p-12 py-16 flex justify-center items-center flex-col'>
       <Title text='PATROCINADORES' />
       <div className='flex mt-8 items-center justify-center gap-12'>
-        <div className='w-[90px]'>
+        <div className='w-[180px]'>
           <img src={SanPetrus} alt="" className='w-full h-full' />
         </div>
-        <div className='w-[80px]'>
+        <div className='w-[160px]'>
           <img src={DentalNorte} alt="" className='w-full h-full' />
         </div>
       </div>
