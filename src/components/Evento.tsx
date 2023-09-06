e8e8ba0744349a5de6a4204a61d105cc48f4ed7f
thiagomello23 <thiagomellodiniz@hotmail.com>
2023-09-06 13:59:06 -0400

    update: Adicionando informacoes a mais no cronograma

--- src/components/Evento.tsx
+++ src/components/Evento.tsx
@@ -1,16 +1,19 @@
 import {} from 'react'
 
-export default function Evento({bgImage, title, data}: {bgImage: string, title: string, data: string}) {
+export default function Evento({bgImage, title, data, body}: {bgImage: string, title: string, data: string, body: string}) {
   return (
     <div 
       style={{backgroundImage: `url(${bgImage})`, backgroundSize: "cover", backgroundRepeat: "no-repeat"}} 
-      className='p-6 w-full lg:w-[20%] h-[170px] lg:h-[200px] flex justify-center items-center text-white rounded-md'
+      className='p-6 w-full lg:w-[20%] h-[170px] lg:h-[200px] flex justify-center items-center flex-col text-white rounded-md'
     >
       <h1 className='text-3xl text-center'>
         {title}
         <br />
         {data}
       </h1>
+      <p className='text-center mt-2 px-2'>
+        {body}
+      </p>
     </div>
   )
 }
