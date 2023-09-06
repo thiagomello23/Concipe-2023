e5e063af1fbbd4f360d9dd489d45282f74a6b3bb
thiagomello23 <thiagomellodiniz@hotmail.com>
2023-09-06 08:21:06 -0400

    update: Adicionando patrocinadores e carrosel

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,16 @@ import Hero from './components/Hero'
 import Palestra from './components/Palestra'
 import { Carousel } from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import EventoAnterior from "./assets/evento-anterior.png"
 import Cronograma from './components/Cronograma';
 import Patrocinio from './components/Patrocinio';
 import Localizacao from './components/Localizacao';
 import Footer from "./components/Footer"
+
+import Banner1 from "./assets/banner-CONCIPE-SITE-1.png"
+import Banner2 from "./assets/banner-CONCIPE-SITE-2.png"
+import Banner3 from "./assets/banner-CONCIPE-SITE-3.png"
+import Banner4 from "./assets/banner-CONCIPE-SITE-4.png"
+import Banner5 from "./assets/banner-CONCIPE-SITE-5.png"
 
 function App() {
   return (
@@ -16,16 +21,32 @@ function App() {
       <Palestra />
       <div className='w-full bg-gray-light'>
         {/* infiniteLoop={true} autoPlay={true} */}
-        <div className='w-[93%] md:w-[93%] lg:w-[84%] m-auto lg:p-2 xl:p-0'>
-          <Carousel swipeScrollTolerance={50} preventMovementUntilSwipeScrollTolerance={true} emulateTouch={true} showThumbs={false} showStatus={false} stopOnHover={true} interval={5000} infiniteLoop={true} autoPlay={true}>
-            <div className='h-[600px] object-cover'>
-              <img src={EventoAnterior} alt="" className='w-full h-full' />
+        <div className='w-[93%] md:w-[93%] lg:w-[100%] xl:w-[84%] m-auto lg:p-2 xl:p-0'>
+          <Carousel 
+            swipeScrollTolerance={50} 
+            preventMovementUntilSwipeScrollTolerance={true} 
+            emulateTouch={true} 
+            showThumbs={false} 
+            showStatus={false} 
+            stopOnHover={true} 
+            interval={5000} 
+            infiniteLoop={true} 
+            autoPlay={true}
+          >
+            <div className='h-[230px] md:h-[400px] xl:h-[670px] lg:h-[560px] object-cover w-full'>
+              <img src={Banner1} alt="" className='w-full h-full' />
             </div>
-            <div className='h-[600px] object-cover'>
-              <img src={EventoAnterior} alt="" className='w-full h-full' />
+            <div className='h-[230px] md:h-[400px] xl:h-[670px] lg:h-[560px] object-cover w-full'>
+              <img src={Banner2} alt="" className='w-full h-full' />
             </div>
-            <div className='h-[600px] object-cover'>
-              <img src={EventoAnterior} alt="" className='w-full h-full' />
+            <div className='h-[230px] md:h-[400px] xl:h-[670px] lg:h-[560px] object-cover w-full'>
+              <img src={Banner3} alt="" className='w-full h-full' />
+            </div>
+            <div className='h-[230px] md:h-[400px] xl:h-[670px] lg:h-[560px] object-cover w-full'>
+              <img src={Banner4} alt="" className='w-full h-full' />
+            </div>
+            <div className='h-[230px] md:h-[400px] xl:h-[670px] lg:h-[560px] object-cover w-full'>
+              <img src={Banner5} alt="" className='w-full h-full' />
             </div>
           </Carousel>
         </div>
--- src/components/Patrocinio.tsx
+++ src/components/Patrocinio.tsx
@@ -1,10 +1,20 @@
 import {} from 'react'
 import Title from './Title'
+import SanPetrus from "../assets/logo-San-Petrus.png"
+import DentalNorte from "../assets/logo-Dental-norte.png"
 
 export default function Patrocinio() {
   return (
-    <div className='bg-white p-12'>
+    <div className='bg-white p-12 flex justify-center items-center flex-col'>
       <Title text='PATROCINADORES' />
+      <div className='flex mt-8 items-center justify-center gap-12'>
+        <div className='w-[90px]'>
+          <img src={SanPetrus} alt="" className='w-full h-full' />
+        </div>
+        <div className='w-[80px]'>
+          <img src={DentalNorte} alt="" className='w-full h-full' />
+        </div>
+      </div>
     </div>
   )
 }
